dfc948648abd8825da032c371bebbf255c6c3593
vamwell <80237011+vamwell@users.noreply.github.com>
2021-03-07 22:22:19 -0600

    Add files via upload

---
+++ geoloc.py
@@ -0,0 +1,27 @@
+# Script written by vamwell 3/7/21
+import requests
+from os import system, name
+
+def clear():
+    if name == "nt":
+        _ = system('cls')
+    else:
+        _ = system('clear')
+
+clear()
+print("Enter IP Below (example: 1.1.1.1)\nOr Leave Empty To Geolocate Your IP.")
+ip = input(">> ")
+clear()
+r = requests.get("http://ip-api.com/json/" + ip + "?fields=1245183")
+print("IP:           " + r.json()['query'])
+print("Continent:    " + r.json()['continent'])
+print("Country:      " + r.json()['country'])
+print("Country Code: " + r.json()['countryCode'])
+print("Timezone:     " + r.json()['timezone'])
+print("City:         " + r.json()['city'])
+print("Zip:          " + r.json()['zip'])
+print("ISP:          " + r.json()['isp'])
+print("Lat:          " + str(r.json()['lat']))
+print("Lon:          " + str(r.json()['lon']))
+print("Reverse DNS:  " + r.json()['reverse'])
+print("Proxy:        " + str(r.json()['proxy']))
